c43bc4030e96c22688b0b96963539ca761d39772
Illya Miloserdov <illyamiloserdov@skaut.cz>
2024-03-08 21:32:41 +0100

    code_fixes

--- main.py
+++ main.py
@@ -2,28 +2,27 @@ import discord
 import toml
 
 class MyClient(discord.Client):
-		async def on_ready(self):
-			print(f'Logged on as {self.user}!')
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
 
-		async def on_message(self, message):
-            print(f'{message.author}: {message.content}')
-
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
 
 def load_config(config_name = "config.toml"):
-	with open(config_name, "r") as f:
-		data = toml.load(f)
+    with open(config_name, "r") as f:
+        data = toml.load(f)
 
-		token = data['token']['token']
-	return token
+        token = data['token']['token']
+    return token
 
 def main():
-	token = load_config()
+    token = load_config()
 
-	intents = discord.Intents.default()
-	intents.message_content = True
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-	client = MyClient(intents=intents)
-	client.run(token)
+    client = MyClient(intents=intents)
+    client.run(token)
 
 if __name__ == "__main__":
-	main()
+    main()
