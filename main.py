f747aa175399dafc00fceee91793729695e32a23
Illya <illyamiloserdov@skaut.cz>
2024-03-19 22:24:07 +0100

    code_updated

--- main.py
+++ main.py
@@ -41,7 +41,8 @@ async def commands(ctx):
         title="**Commands you are or will be able to use**",
         description="**.hello** - Introduction of bot\n"
                     "**.commands** - Showing all commands of a bot\n"
-                    "**.cs_elo** <nickmane> - Show elo on faceit of certain player"
+                    "**.cs2_elo** <nickmane> - Show elo on cs2 faceit of certain player\n"
+                    "**.csgo_elo** <nickmane> - Show elo on csgo faceit of certain player"
     )
     embed.set_author(name="1llya's bot")
     embed.set_thumbnail(url=bot_avatar_img)
@@ -49,9 +50,64 @@ async def commands(ctx):
     await ctx.send(embed=embed)
 
 @bot.command()
-async def cs_elo(ctx, nickname: str):
+async def cs2_elo(ctx, nickname: str):
     # cs2 data fetch
     url = f"https://api.satont.ru/faceit?nick={nickname}&game=cs2&timezone=Europe%2FPrague"
+    cs_elo_data = requests.get(url)
+    cs_elo_data = cs_elo_data.json()
+    # stats
+    elo = int(cs_elo_data['elo'])
+    kd = cs_elo_data['stats']['lifetime']['Average K/D Ratio']
+    winrate = cs_elo_data['stats']['lifetime']['Win Rate %']
+    matches = cs_elo_data['stats']['lifetime']['Matches']
+    wins = cs_elo_data['stats']['lifetime']['Wins']
+    loses = int(matches) - int(wins)
+    # TODO Check response status code example if status code is not 200 tell about it to user of bot
+
+
+
+    # embed creation
+    embed = discord.Embed(
+        colour=discord.colour.parse_hex_number("ff0008"),
+        title=f"**{nickname}**",
+        url=f"https://faceit.com/en/players/{nickname}",
+        description=f"**Elo** - {str(elo)}\n"
+                    f"**Avg. K/D** - {kd}\n"
+                    f"**Matches** - {matches}\n"
+                    f"**Wins** - {wins}\n"
+                    f"**Loses** - {loses}\n"
+                    f"**Winrate** - {winrate}%"
+    )
+    # determinates thumbnail image
+    if elo > 2001:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit10.png")
+    elif elo > 1751:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit9.png")
+    elif elo > 1531:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit8.png")
+    elif elo > 1351:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit7.png")
+    elif elo > 1201:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit6.png")
+    elif elo > 1051:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit5.png")
+    elif elo > 901:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit4.png")
+    elif elo > 751:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit3.png")
+    elif elo > 501:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit2.png")
+    elif elo > 100:
+        embed.set_thumbnail(url="https://leetify.com/assets/images/rank-icons/faceit1.png")
+
+
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def csgo_elo(ctx, nickname: str):
+    # cs2 data fetch
+    url = f"https://api.satont.ru/faceit?nick={nickname}&game=csgo&timezone=Europe%2FPrague"
     cs_elo_data = requests.get(url)
     cs_elo_data = cs_elo_data.json()
     # stats
