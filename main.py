9b9e94f140cca97b291c4594b6880a9f7e1c357c
Illya Miloserdov <illyamiloserdov@skaut.cz>
2024-03-19 14:51:26 +0100

    Reasembled_code

--- main.py
+++ main.py
@@ -1,55 +1,42 @@
 import discord
+from discord.ext import commands
 import toml
-import requests
 
-api_url = "https://valorant-api.com/v1/agents?isPlayableCharacter=true"
+bot = commands.Bot(command_prefix="-", intents=discord.Intents.all())
+bot_channel_id = 786984140234555402
 
+@bot.event
+async def on_ready():
+    print("Hello, bot is ready!")
+    channel = bot.get_channel(bot_channel_id)
+    await channel.send("Hello bot is ready to work!")
 
-class ValorantAgents:
-    def __init__(self):
-        data = requests.get(api_url)
+@bot.command()
+async def hello(ctx):
+    embed = discord.Embed(
+            colour=discord.colour.parse_hex_number("997b28"),
+            description="Hello, my name is 1llya's\nIm his first bot so be humble\nYou can see my commands by typing **-help**",
+            title="Info about me"
+            )
+    embed.set_footer(text="If you click my name you can see 1llya's Github")
+    embed.set_author(name="1llya", url="https://github.com/1llyaa/discord-bot-Illya")
+    embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/1071136297533579334/1219639688026132553/1llya_AI_avatar_Men_Czech_nationality_has_glassses_on_face_litt_ce1b968d-0ce3-4157-a616-a001c51d8642.png?ex=660c08f9&is=65f993f9&hm=0c5076e7006684155e9a49ffc942b41107a46322d0eb00e18feaa7dff14c6295&")
 
-        self.parsed_json = data.json()
-        # for i in range(len(parsed_json['data'])):
-        #     print(parsed_json['data'][i]['displayName'])
-
-    async def names_agents(self):
-        agents_list = []
-        for i in range(len(self.parsed_json['data'])):
-            agents_list.append(self.parsed_json['data'][i]['displayName'])
-
-        return agents_list
-
-        # return self.parsed_json
-
-
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
+    await ctx.send(embed=embed)
 
 
-def load_config(config_name="config.toml"):
-    with open(config_name, "r") as f:
+def load_config():
+    with open("./config.toml", "r") as f:
         data = toml.load(f)
 
-        token = data['token']['token']
+        token = data['config']['token']
     return token
 
-
 def main():
-
-    token = load_config()
-
-    intents = discord.Intents.default()
-    intents.message_content = True
-
-    client = MyClient(intents=intents)
-    client.run(token)
+    discord_token = load_config()
+    
+    bot.run(discord_token)
 
 
 if __name__ == "__main__":
     main()
-
